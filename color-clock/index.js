7ac31ada7c7cda62ae13117022448cf026bf57e7
Garrett Burns <garrettburns@me.com>
2019-11-15 14:51:28 -0800

    Done.

--- color-clock/index.js
+++ color-clock/index.js
@@ -4,7 +4,8 @@ function displayTime() {
   let clock = document.querySelector('#clock')
   clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
   setInterval(function() {
-      clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
+      console.log('hey');
+      clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a');
       clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
       clock.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
   }, 1000)
